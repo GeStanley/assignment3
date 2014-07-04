e9d1792b21799f194dc31717f7f5320167c2bd53
GeStanley <geoffrey.stanley@gmail.com>
2014-07-03 21:41:11 -0400

    merge resolve

--- Fraction.cpp
+++ Fraction.cpp
@@ -26,12 +26,104 @@ Fraction::Fraction( long numerator, long denominator)
   this->denominator = denominator;
 }
 
-bool operator == (const Fraction &lhs, const Fraction & rhs)
+Fraction::Fraction( const string & input)
 {
-  if(lhs.numerator==rhs.numerator && lhs.denominator==rhs.denominator)
+  string::size_type sz;
+  Fraction(stol(input, &sz));
+}
+
+Fraction Fraction::operator+ (const Fraction& lhs, const Fraction& rhs)
+{
+  Fraction temp(lhs);
+  temp += rhs;
+  return temp;
+}
+
+Fraction Fraction::operator- (const Fraction& lhs, const Fraction& rhs)
+{
+  Fraction temp(lhs);
+  temp -= rhs;
+  return temp;
+}
+
+Fraction & Fraction::operator += (const Fraction & rhs)
+{
+  this->numerator = this->numerator * rhs.denominator
+                   + rhs.numerator * this->denominator;
+  this->denominator *= rhs.denominator;
+
+  return *this;
+}
+
+Fraction & Fraction::operator -= (const Fraction & rhs)
+{
+  this->numerator = this->numerator * rhs.denominator
+                   - rhs.numerator * this->denominator;
+  this->denominator *= rhs.denominator;
+
+  return *this;
+}
+
+bool operator == (const Fraction &lhs, const Fraction &rhs)
+{
+  if(lhs.numerator==rhs.numerator &&
+    lhs.denominator==rhs.denominator)
     return true;
   else
     return false;
+}
+
+bool Fraction::operator < (const Fraction &lhs, const Fraction &rhs)
+{
+  if(lhs.numerator==rhs.numerator)
+  {
+    if(lhs.denominator>rhs.denominator)
+      return true;
+    else
+      return false;
+  }
+  else
+    return false;
+}
+
+bool Fraction::operator != (const Fraction &lhs, const Fraction &rhs)
+{
+  if(lhs == rhs) return false;
+
+  return true;
+}
+
+bool Fraction::operator <= (const Fraction &lhs, const Fraction &rhs)
+{
+  if(lhs == rhs || lhs < rhs)
+    return true;
+
+  return false;
+}
+
+bool Fraction::operator > (const Fraction &lhs, const Fraction &rhs)
+{
+  if(lhs < rhs) return false;
+
+  if(lhs == rhs) return false;
+
+  return true;
+}
+
+bool Fraction::operator >= (const Fraction &lhs, const Fraction &rhs)
+{
+  if(lhs < rhs) return false;
+
+  return true;
+}
+
+istream & operator << (istream &input, Fraction &fraction)
+{
+    //input >> must finish this to extract numerator
+    input.ignore(); //ignore the /
+    //input >> must finish this to extract denominator
+    //pass numer and denom to fraction.
+    return input;
 }
 
 ostream & operator << (ostream &output, const Fraction &fraction)
--- Fraction.h
+++ Fraction.h
@@ -15,30 +15,30 @@ public:
   Fraction();
   Fraction( long);
   Fraction( long, long);
-  //Fraction( std::string);
+  Fraction( const string &);
 
   //unary operators
   //Fraction operator+ (const Fraction & rhs);
   //Fraction operator- (const Fraction & rhs);
   //binary operators
-  //Fraction operator+ (const Fraction& lhs, const Fraction& rhs);
-  //Fraction operator- (const Fraction& lhs, const Fraction& rhs);
+  Fraction operator+ (const Fraction &, const Fraction &);
+  Fraction operator- (const Fraction &, const Fraction &);
   //Fraction operator* (const Fraction& lhs, const Fraction& rhs);
   //Fraction operator/ (const Fraction& lhs, const Fraction& rhs);
 
-  //Fraction & Fraction::operator += (const Fraction & rhs);
-  //Fraction & Fraction::operator -= (const Fraction & rhs);
+  Fraction & operator += (const Fraction &);
+  Fraction & operator -= (const Fraction &);
   //Fraction & Fraction::operator *= (const Fraction & rhs);
   //Fraction & Fraction::operator /= (const Fraction & rhs);
 
   //main logical operators
   friend bool operator == (const Fraction &, const Fraction &);
-  //bool operator < (const Fraction &lhs, const Fraction & rhs);
+  bool operator < (const Fraction &, const Fraction &);
   //secondary logical operators, deduced from above
-  //bool operator != (const Fraction &lhs, const Fraction & rhs);
-  //bool operator <= (const Fraction &lhs, const Fraction & rhs);
-  //bool operator > (const Fraction &lhs, const Fraction & rhs);
-  //bool operator >= (const Fraction &lhs, const Fraction & rhs);
+  bool operator != (const Fraction &lhs, const Fraction & rhs);
+  bool operator <= (const Fraction &lhs, const Fraction & rhs);
+  bool operator > (const Fraction &lhs, const Fraction & rhs);
+  bool operator >= (const Fraction &lhs, const Fraction & rhs);
 
   //Fraction & Fraction::operator++(); //++f
   //Fraction Fraction::operator++(int); //f++
@@ -47,6 +47,7 @@ public:
   //String operator () ();
 
   //insertion operator >>
+  friend istream & operator << ( istream &, Fraction &);
 
   //extraction operator <<
   friend ostream & operator << ( ostream &, const Fraction &);
